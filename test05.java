aa64120a1ab658d8d767deb4800162f36ee1a853
Arvind_Kokane <kokanearvind875@gmail.com>
2026-03-14 14:48:09 +0530

    Refactor Main class and clean up code
    Removed unused variable 'num' initialization and added a newline at the end of the file.

--- test05.java
+++ test05.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int n = 5;
-        int num = 1;
+       
 
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= i; j++) {
